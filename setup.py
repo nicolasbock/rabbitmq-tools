0f577db41cb33eab63508efaeac04b99abec27e2
Nicolas Bock <nicolas.bock@canonical.com>
2020-09-01 11:12:55 -0600

    Remove old script from setup
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setuptools.setup(
     name="RabbitMQ Test",
     verion="1",
     packages=setuptools.find_packages(),
-    scripts=["test-rabbit.py"],
     entry_points={
         "console_scripts": [
             "rabbitmq-tools = rabbitmqtools.main:main",
