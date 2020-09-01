53c2deb031f0dc53237220fdaed65908a0e8337e
Nicolas Bock <nicolas.bock@canonical.com>
2020-09-01 11:07:32 -0600

    Add tests for command line parsing
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -1,7 +1,23 @@
 import unittest
+import sys
+
+try:
+    # python 3.4+ should use builtin unittest.mock not mock package
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from rabbitmqtools import commandline
 
 
 class TestCommandLine(unittest.TestCase):
 
-    def testDummy(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.args = ["rabbit-tools", "127.0.0.1"]
+
+    def testBasic(self):
+        with patch.object(sys, 'argv', self.args):
+            options = commandline.parse_command_line()
+            self.assertTrue(isinstance(options.BROKER, list))
+            self.assertEquals(len(options.BROKER), 1)
+            self.assertEquals(options.BROKER[0], "127.0.0.1")
