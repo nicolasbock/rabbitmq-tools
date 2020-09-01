cfafe944a064f159edbb815969edcf34df83f8b1
Nicolas Bock <nicolas.bock@canonical.com>
2020-09-01 12:32:25 -0600

    Version bump to v1.0.2
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="RabbitMQ Test Tools",
-    version="1.0.1",
+    version="1.0.2",
     description="This is a test",
     long_description="...",
     long_description_content_type="text/markdown",
