25a990f2922fbb5d6425b3e551817a791201399d
Nicolas Bock <nicolas.bock@canonical.com>
2020-09-01 10:38:37 -0600

    Refactor
    Break up the existing code into modules.
    
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ import setuptools
 setuptools.setup(
     name="RabbitMQ Test",
     verion="1",
-    scripts=["test-rabbit.py"]
+    packages=setuptools.find_packages(),
+    scripts=["test-rabbit.py"],
+    entry_points={
+        "console_scripts": [
+            "rabbitmq-tools = rabbitmqtools.main:main",
+        ],
+    }
 )
